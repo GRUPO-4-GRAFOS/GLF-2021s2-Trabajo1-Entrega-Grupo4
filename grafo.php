6e19ad64999fa072b15fd4f39609b0c7d2f8e6db
HoodieMcFly <nico.2010.mdp@gmail.com>
2021-09-11 13:28:43 -0300

    modificación archivo grafo.php, matriz modular

--- grafo.php
+++ grafo.php
@@ -5,26 +5,21 @@ if ($n != NULL){
     
     $fi=fopen("archivo.log", "a");
     //or die("problemas al crear archivo.txt");
-     fwrite($fi, "Datos: ");
-     fwrite($fi, "\n");
-     fwrite($fi, "La cantidad de nodos es: ");
+     fwrite($fi, "[grafo.php]El usuario ha seleccionado la cantidad de nodos: ");
      fwrite($fi, $n);
      fwrite($fi, "\n");
-     fwrite($fi, "El tipo de grafo es: ");
+     fwrite($fi, "[grafos.php]El usuario ha seleccionado el tipo de grafo: ");
      fwrite($fi,$a);
      fwrite($fi, "\n");
-     fwrite($fi,"-------------------- \n\n");
      fclose($fi);
    }
 
 else {
-    echo "Dato incompleto, vuelva y rellene la casilla";
+    echo "Datos incompletos o no seleccionados, vuelva y rellene la casilla";
 }
 
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,65 +41,28 @@ else {
         <input type="submit" value="Enviar"/>
 
 <table>
-    <tr>
-        <td> </td>
-        <td>ABCDE</td>
-    </tr>  
-    <tr>
-        <td>A</td>
-        <td><?php
-        $bin1 = $_POST['Conectado'] - 1;
-        $bin2 = $_POST ['Conectado1'] - 1 ; 
-        $Matriz = array (Array (0,0,0,0,0), Array (0,0,0,0,0), Array (0,0,0,0,0) , Array (0,0,0,0,0) , Array (0,0,0,0,0));
-        $Matriz[$bin1][$bin2]  = 1;
-        $i=0;
-            for($j= 0; $j <5; $j++){
-                echo " " .$Matriz[$i][$j];
-            }   
-        ?> </td>
-    </tr>
-    <tr>
-        <td>B</td>
-        <td><?php
-        $bin1 = $_POST['Conectado'] - 1;
-        $bin2 = $_POST ['Conectado1'] - 1 ; 
-        $Matriz = array (Array (0,0,0,0,0), Array (0,0,0,0,0), Array (0,0,0,0,0) , Array (0,0,0,0,0) , Array (0,0,0,0,0));
-        $Matriz[$bin1][$bin2]  = 1;
-        $i=1;
-            for($j= 0; $j <5; $j++){
-                echo " " .$Matriz[$i][$j];
-            }   
-        ?> </td>
-    </tr>
-    <tr>
-        <td>C</td>
-        <td><?php
-        $bin1 = $_POST['Conectado'] - 1;
-        $bin2 = $_POST ['Conectado1'] - 1 ; 
-        $Matriz = array (Array (0,0,0,0,0), Array (0,0,0,0,0), Array (0,0,0,0,0) , Array (0,0,0,0,0) , Array (0,0,0,0,0));
-        $Matriz[$bin1][$bin2]  = 1;
-        $i=2;
-            for($j= 0; $j <5; $j++){
-                echo " " .$Matriz[$i][$j];
-            }   
-        ?> </td>
-    </tr>
-    <tr>
-        <td>D</td>
-        <td><?php
-        $bin1 = $_POST['Conectado'] - 1;
-        $bin2 = $_POST ['Conectado1'] - 1 ; 
-        $Matriz = array (Array (0,0,0,0,0), Array (0,0,0,0,0), Array (0,0,0,0,0) , Array (0,0,0,0,0) , Array (0,0,0,0,0));
-        $Matriz[$bin1][$bin2]  = 1;
-        $i=3;
-            for($j= 0; $j <5; $j++){
-                echo " " .$Matriz[$i][$j];
-            }   
-        ?> </td>
-    </tr>
-    <tr>
+
+    <?php
+        for($i = 0; $i < $n; $i++){
+            ?>
+            <tr>
+            <?php
+            for($j = 0; $j < $n; $j++){
+                ?>
+                    <td>
+                        <?php echo $i ?> <!-- aca va el dato que se escribe dentro de cada cuadrito de la matriz-->
+                    </td>
+                <?php
+            }
+            ?>
+            </tr>
+            <?php
+        }
+    ?>
+    
+    <!-- <tr>
         <td>E</td>
-        <td><?php
+        <td><?php/*
         $bin1 = $_POST['Conectado'] - 1;
         $bin2 = $_POST ['Conectado1'] - 1 ; 
         $Matriz = array (Array (0,0,0,0,0), Array (0,0,0,0,0), Array (0,0,0,0,0) , Array (0,0,0,0,0) , Array (0,0,0,0,0));
@@ -113,7 +71,7 @@ else {
             for($j= 0; $j <5; $j++){
                 echo " " .$Matriz[$i][$j];
             }   
-        ?> </td>
+        */?> </td>
     </tr>
 </table>
 </body>
