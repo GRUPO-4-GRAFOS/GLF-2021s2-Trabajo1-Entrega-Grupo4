7bb13edec4bbeb06fefe06084a9edf7c103a9045
Lupapiche3 <89411116+Lupapiche3@users.noreply.github.com>
2021-09-15 18:42:34 -0300

    Add files via upload

--- grafo.php
+++ grafo.php
@@ -1,173 +1,211 @@
 <?php
-  function implodeArrayofArrays($array, $glue  = ', ') {
-    $output = '';
-    foreach ($array as $subarray) {
-        $output .= implode('-', $subarray);
+error_reporting(0);
+    $s=0; $i=0; $j=0;
+    $n=array();
+    $bin= array();
+    if(isset($_POST ["MatrizNodo"])){
+        $n[0][0]= (int)$_POST ["txtn00"];
+        $n[0][1]= (int)$_POST ["txtn01"];
+        $n[0][2]= (int)$_POST ["txtn02"];
+        $n[0][3]= (int)$_POST ["txtn03"];
+        $n[0][4]= (int)$_POST ["txtn04"];
+
+        $n[1][0]= (int)$_POST ["txtn10"];
+        $n[1][1]= (int)$_POST ["txtn11"];
+        $n[1][2]= (int)$_POST ["txtn12"];
+        $n[1][3]= (int)$_POST ["txtn13"];
+        $n[1][4]= (int)$_POST ["txtn14"];
+
+        $n[2][0]= (int)$_POST ["txtn20"];
+        $n[2][1]= (int)$_POST ["txtn21"];
+        $n[2][2]= (int)$_POST ["txtn22"];
+        $n[2][3]= (int)$_POST ["txtn23"];
+        $n[2][4]= (int)$_POST ["txtn24"];
+
+        $n[3][0]= (int)$_POST ["txtn30"];
+        $n[3][1]= (int)$_POST ["txtn31"];
+        $n[3][2]= (int)$_POST ["txtn32"];
+        $n[3][3]= (int)$_POST ["txtn33"];
+        $n[3][4]= (int)$_POST ["txtn34"];
+
+        $n[4][0]= (int)$_POST ["txtn30"];
+        $n[4][1]= (int)$_POST ["txtn31"];
+        $n[4][2]= (int)$_POST ["txtn32"];
+        $n[4][3]= (int)$_POST ["txtn33"];
+        $n[4][4]= (int)$_POST ["txtn34"];
     }
-    return $output;
-  }
-
-  error_reporting(E_ALL ^ E_NOTICE);
-
-  include './models/log.php';
-
-  $nodoss = "./assets/graph_dataset.txt"; 
-  $matrizz = "./assets/matriz_dataset.txt"; 
-
-  $ini = 0;
-  $ter = 0;
-  $num = 0;
-
-  $ini = $_POST["inicio_arista"];
-  $ter = $_POST["termino_arista"];
-
-  if($ini == NULL || $ter == NULL){
-    $ini = 0;
-    $ter = 0;
-  }
-
-  if($num == 0){
-    $num = $_POST["cantnodo"];
-    $tipoGrafo = $_POST["tipografo"];
-  }
-
-  $archivo = file($nodoss);
-
-  if($num == 0){
-    $num = intval($archivo[1]);
-  }
-
-  $fp = fopen($nodoss, "a");
-  fwrite($fp, "Cantidad de nodos:");
-  fwrite($fp, "\n");
-  fwrite($fp, $num);
-  fwrite($fp, "\n");
-  fwrite($fp, "Tipo:");
-  fwrite($fp, "\n");
-  fwrite($fp, $tipoGrafo);
-  fclose($fp);
-
-  if ($num != NULL){
-    $log = new Log('./log/loginfo.log');
-    $log->writeline('Info', "[grafo.php]El usuario ha seleccionado la cantidad de nodos: $num ");
-    $log->writeline('Info', "[grafo.php]El usuario ha seleccionado el tipo de grafo: $tipoGafo \n");
-    $log->close();
-  }
-
-  for($i = 0; $i <= 0; $i++){
-    for($j = 0; $j <= 0; $j++){
-      $matriz[$i][$j] = 0;
+    for($i=0; $i<5; $i++){
+        for($j=0; $j<5; $j++){
+            $bin[$i][$j]=$n[$i][$j];
+        }
     }
-  }
-
-  if($ini != 0 && $ter != 0){
-
-    echo implodeArrayofArrays($matriz);
-
-    $ff = fopen($matrizz, "a");
-    fwrite($ff,implodeArrayofArrays($matriz));
-
-    $matriz[$ini][$ter] = 1;
-    $matriz[$ter][$ini] = 1;
-
-  }
-
-  $n = $num;
-
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
-  <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
-    <meta 
-      http-equiv="X-UA-Compatible" 
-      content="IE=edge"
-    >
-    <meta 
-      name="viewport" 
-      content="width=device-width, initial-scale=1.0"
-    >
-    <link 
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
-      rel="stylesheet"
-      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
-      crossorigin="anonymous"
-    >
-    <link rel="stylesheet" href="./css/styles.css">
-    <title>Trabajo 1</title>
-  </head>
-  <body>
-  <div class="titulo">
-          <h1>Trabajo N°1 Grafo</h1>
-    </div>
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-sm-6">
-          <form action="grafo.php" method="post">
-            Nodo:
-            <input 
-              type="number" 
-              name="inicio_arista" 
-              class="form-control" 
-              min="0" 
-              max="10" 
-            />
-            Conecta con:
-            <input 
-              type="number" 
-              name="termino_arista" 
-              class="form-control" 
-              min="0" 
-              max="10"
-            />
-            <input 
-              type="submit" 
-              value="Enviar" 
-              class="btn btn-primary btn-lg my-3"
-            />
-          </form>
-        </div>
-        <div class="col-sm-6">
-          <table class="table table-primary">
-           
-           
-           
-           
-           
-           <?php       
-              $cont = 0;
-              for($i = 0; $i <= $n; $i++){
-                echo'<tr>';
-                for($j = 0; $j <= $n; $j++){
-                  echo '<td>';
-                  if($i == 0){
-                    echo $cont;
-                    $cont++;
-                  }else{
-                    if($cont == $n+1){
-                      $cont = 1;
-                    }
-                    if($j == 0){
-                      echo $cont;
-                      $cont++;
-                    }
-                  }
-                  if($i != 0 && $j != 0){
-                    if($matriz[$i][$j] == NULL){
-                      echo 0;
-                    }else{
-                      echo $matriz[$i][$j];
-                    }
-                  }        
-                  echo '</td>';
-                } 
-                echo'</tr>';
-              }
-            ?>
-          </table>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table, tr, td{ 
+            border: 1px solid;
+        }
+    </style>
+</head>
+<body>
+<form method="post" action="grafo.php">
+<table width="115" border="0"> 
+    <tr>
+        <td colspan="6" style="text-align: center;" ><strong >ESCRIBIR PESO DE CONEXION</td>
+    </tr>
+    <tr>
+        <td>  </td>
+        <td> A </td>
+        <td> B </td>
+        <td> C </td>
+        <td> D </td>
+        <td> E </td>
+    </tr>
+    <tr>
+        <td width="43"> A </td>
+        <td width="43"><input name="txtn00" type="text" id="txtn00"
+        value="<?= $n[0][0]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn01" type="text" id="txtn01"
+        value="<?= $n[0][1]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn02" type="text" id="txtn02"
+        value="<?= $n[0][2]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn03" type="text" id="txtn03"
+        value="<?= $n[0][3]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn04" type="text" id="txtn04"
+        value="<?= $n[0][4]?>" size = "5"/> </td>
+
+    </tr>
+    <tr>
+        <td width="43"> B </td>
+        <td width="43"><input name="txtn10" type="text" id="txtn10"
+        value="<?= $n[1][0]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn11" type="text" id="txtn11"
+        value="<?= $n[1][1]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn12" type="text" id="txtn12"
+        value="<?= $n[1][2]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn13" type="text" id="txtn13"
+        value="<?= $n[1][3]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn14" type="text" id="txtn14"
+        value="<?= $n[1][4]?>" size = "5"/> </td>
+
+    </tr>
+    <tr>
+        <td width="43"> C </td>
+        <td width="43"><input name="txtn20" type="text" id="txtn20"
+        value="<?= $n[2][0]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn21" type="text" id="txtn21"
+        value="<?= $n[2][1]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn22" type="text" id="txtn22"
+        value="<?= $n[2][2]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn23" type="text" id="txtn23"
+        value="<?= $n[2][3]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn24" type="text" id="txtn24"
+        value="<?= $n[2][4]?>" size = "5"/> </td>
+    </tr>
+
+    <tr>
+        <td width="43"> D </td>
+        <td width="43"><input name="txtn30" type="text" id="txtn30"
+        value="<?= $n[3][0]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn31" type="text" id="txtn31"
+        value="<?= $n[3][1]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn32" type="text" id="txtn32"
+        value="<?= $n[3][2]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn33" type="text" id="txtn33"
+        value="<?= $n[3][3]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn34" type="text" id="txtn34"
+        value="<?= $n[3][4]?>" size = "5"/> </td>
+    </tr>
+    <tr>
+        <td width="43"> E </td>
+        <td width="43"><input name="txtn40" type="text" id="txtn40"
+        value="<?= $n[4][0]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn41" type="text" id="txtn41"
+        value="<?= $n[4][1]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn42" type="text" id="txtn42"
+        value="<?= $n[4][2]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn43" type="text" id="txtn43"
+        value="<?= $n[4][3]?>" size = "5"/> </td>
+        <td width="62"><input name="txtn44" type="text" id="txtn44"
+        value="<?= $n[4][4]?>" size = "5"/> </td>
+    </tr>
+    <tr></tr>
+    <tr>
+        <td>
+            <input name= "MatrizNodo" type="submit" id="MatrizNodo" value="Mostrar"/></td>
+        </td>
+    </tr>
+</table>
+<?php
+      for($i=0; $i<5; $i++){
+        for($j=0; $j<5; $j++){
+            if($bin[$i][$j]>0){
+                $bin[$i][$j]=1;
+            }
+        }
+    }
+?>
+<table width="115" border="0"> 
+    <tr>
+        <td colspan="6" style="text-align: center;" ><strong >Matriz De Adyacencia</td>
+    </tr>   
+    <tr>
+        <td>  </td>
+        <td> A </td>
+        <td> B </td>
+        <td> C </td>
+        <td> D </td>
+        <td> E </td>
+    </tr>
+    <tr>
+        <td width="43"> A </td>
+        <td><?php echo $bin[0][0];?></td>
+        <td><?php echo $bin[0][1];?></td>
+        <td><?php echo $bin[0][2];?></td>
+        <td><?php echo $bin[0][3];?></td>
+        <td><?php echo $bin[0][4];?></td>
+
+    </tr>
+    <tr>
+        <td width="43"> B </td>
+        <td><?php echo $bin[1][0];?></td>
+        <td><?php echo $bin[1][1];?></td>
+        <td><?php echo $bin[1][2];?></td>
+        <td><?php echo $bin[1][3];?></td>
+        <td><?php echo $bin[1][4];?></td>
+
+    </tr>
+    <tr>
+        <td width="43"> C </td>
+        <td><?php echo $bin[2][0];?></td>
+        <td><?php echo $bin[2][1];?></td>
+        <td><?php echo $bin[2][2];?></td>
+        <td><?php echo $bin[2][3];?></td>
+        <td><?php echo $bin[2][4];?></td>
+    </tr>
+
+    <tr>
+        <td width="43"> D </td>
+        <td><?php echo $bin[3][0];?></td>
+        <td><?php echo $bin[3][1];?></td>
+        <td><?php echo $bin[3][2];?></td>
+        <td><?php echo $bin[3][3];?></td>
+        <td><?php echo $bin[3][4];?></td>
+    </tr>
+    <tr>
+        <td width="43"> E </td>
+        <td><?php echo $bin[4][0];?></td>
+        <td><?php echo $bin[4][1];?></td>
+        <td><?php echo $bin[4][2];?></td>
+        <td><?php echo $bin[4][3];?></td>
+        <td><?php echo $bin[4][4];?></td>
+    </tr>
+</form>
+ 
